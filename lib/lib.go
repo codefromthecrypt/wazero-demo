679710497536ba84f8bc0613aba0583d7d7eb5e3
birros <birros@protonmail.com>
2022-03-25 12:13:24 +0100

    style(lib): use of the 80 character rule

--- lib/lib.go
+++ lib/lib.go
@@ -17,11 +17,16 @@ func Start() {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	// Assign the Go context to the runtime, so it is used during instantiation and any function calls.
-	runtime := wazero.NewRuntimeWithConfig(wazero.NewRuntimeConfig().WithContext(ctx))
+	// Assign the Go context to the runtime, so it is used during instantiation
+	// and any function calls.
+	runtime := wazero.NewRuntimeWithConfig(
+		wazero.NewRuntimeConfig().WithContext(ctx),
+	)
 
-	// sum.wasm was compiled with TinyGo, which requires being instantiated as a WASI command (to initialize memory).
-	// This is required by TinyGo even if the source (../sum/sum.go in this case) doesn't directly use I/O or memory.
+	// sum.wasm was compiled with TinyGo, which requires being instantiated as a
+	// WASI command (to initialize memory).
+	// This is required by TinyGo even if the source (../sum/sum.go in this
+	// case) doesn't directly use I/O or memory.
 	wasi, err := runtime.InstantiateModule(wazero.WASISnapshotPreview1())
 	if err != nil {
 		log.Panicln(err)
